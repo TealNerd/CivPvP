7b3ca17098f85b592257fd87ca8f9df30982ff87
maxopoly <maxseinbot@gmx.de>
2015-10-29 18:05:46 +0100

    Less performance intensive rank calculating

--- src/com/gimmicknetwork/civpvpinventory/DuelManager.java
+++ src/com/gimmicknetwork/civpvpinventory/DuelManager.java
@@ -5,6 +5,9 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
@@ -20,6 +23,7 @@ public class DuelManager {
 	private HashMap<UUID, Integer> elo = new HashMap<UUID, Integer>();
 	private HashMap<UUID, UUID> runningDuels = new HashMap<UUID, UUID>();
 	private HashMap<UUID, UUID> requestedDuels = new HashMap<UUID, UUID>();
+	private LinkedList<UUID> ranks = new LinkedList<UUID>();
 	private Database db;
 
 	public DuelManager(CivpvpInventory plugin, Database db) {
@@ -30,19 +34,56 @@ public class DuelManager {
 	public void loadElos() {
 		try {
 			db.execute("CREATE TABLE IF NOT EXISTS elos (uuid VARCHAR(40) not null unique, elo INT(255) not null)");
-			PreparedStatement selectElos = db.prepareStatement("SELECT * FROM elos");
+			PreparedStatement selectElos = db
+					.prepareStatement("SELECT * FROM elos");
 			ResultSet elos = selectElos.executeQuery();
-			while(elos.next()) {
-				elo.put(UUID.fromString(elos.getString("uuid").trim()), elos.getInt("elo"));
+			while (elos.next()) {
+				elo.put(UUID.fromString(elos.getString("uuid").trim()),
+						elos.getInt("elo"));
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+		for (Map.Entry<UUID, Integer> current : elo.entrySet()) {
+			int insertValue = current.getValue();
+			ListIterator<UUID> iter = ranks.listIterator();
+			while (true) {
+				if (!iter.hasNext()) {
+					ranks.addLast(current.getKey());
+					break;
+				}
+				UUID u = iter.next();
+				if (elo.get(u) > insertValue) {
+					iter.previous();
+					iter.add(current.getKey());
+					break;
+				}
+			}
+		}
 	}
 	
+	public void fixRanking(UUID uuid) {
+		ranks.remove(uuid);
+		int insertValue = elo.get(uuid);
+		ListIterator<UUID> iter = ranks.listIterator();
+		while (iter.next() != null) {
+			if (!iter.hasNext()) {
+				ranks.addLast(uuid);
+				break;
+			}
+			UUID u = iter.next();
+			if (elo.get(u) > insertValue) {
+				iter.previous();
+				iter.add(uuid);
+				break;
+			}
+		}
+	}
+
 	public void saveElos() {
 		try {
-			PreparedStatement setElo = db.prepareStatement("REPLACE INTO elos (uuid,elo) VALUES (?,?)");
+			PreparedStatement setElo = db
+					.prepareStatement("REPLACE INTO elos (uuid,elo) VALUES (?,?)");
 			for (Map.Entry<UUID, Integer> current : elo.entrySet()) {
 				setElo.setString(1, current.getKey().toString());
 				setElo.setInt(2, current.getValue());
@@ -56,25 +97,16 @@ public class DuelManager {
 	public Integer getElo(Player p) {
 		return elo.get(p.getUniqueId());
 	}
-	
+
 	public int getRank(Player p) {
-		ArrayList<Integer> values = new ArrayList<Integer>();
-		for(int i : elo.values()) {
-			if(!values.contains(i)) {
-				values.add(i);
-			}
-		}
-		Collections.sort(values);
-		int size = values.size();
-		int playerElo = elo.get(p.getUniqueId());
-		int index = values.indexOf(playerElo);
-		return size - index;
+		return ranks.indexOf(p.getUniqueId());
 	}
 
 	public void checkElo(Player p) {
-		p.sendMessage(ChatColor.GOLD + "Your elo is " + getElo(p) + ", this makes your rank: " + getRank(p));
+		p.sendMessage(ChatColor.GOLD + "Your elo is " + getElo(p)
+				+ ", this makes your rank: " + getRank(p));
 	}
-	
+
 	public void startDuel(Player first, Player second) {
 		if (first != null && second != null) {
 			runningDuels.put(first.getUniqueId(), second.getUniqueId());
@@ -106,11 +138,13 @@ public class DuelManager {
 				+ ". You gained " + eloDif + " elo, total now: "
 				+ (elo.get(winnerUUID) + eloDif));
 		elo.put(winnerUUID, elo.get(winnerUUID) + eloDif);
+		fixRanking(winnerUUID);
 		UUID loserUUID = loser.getUniqueId();
 		loser.sendMessage("You lost against " + winner.getName()
 				+ ". You lost " + eloDif + " elo, total now: "
 				+ (elo.get(loserUUID) - eloDif));
 		elo.put(loserUUID, elo.get(loserUUID) - eloDif);
+		fixRanking(loserUUID);
 		plugin.getLogger().log(
 				Level.INFO,
 				winner.getName() + "won against " + loser.getName()
@@ -158,7 +192,7 @@ public class DuelManager {
 	public boolean wasRequested(Player p) {
 		return requestedDuels.containsKey(p.getUniqueId());
 	}
-	
+
 	public boolean areDueling(Player a, Player b) {
 		return runningDuels.get(a.getUniqueId()) == b.getUniqueId();
 	}
